9a96b66cb5f97e9038d7c07a581c5bd857a54566
binhang <yuanbinhang@hotmail.com>
2022-03-14 23:38:57 +0100

    foo update

--- utils/dist_debug_utils.py
+++ utils/dist_debug_utils.py
@@ -5,6 +5,6 @@ def print_cuda_memory(args, info: str, device=None):
     if args.debug_mem:
         if device is None:
             device = torch.device('cuda', args.cuda_id)
-        print("<{}>: current memory allocated: {:2.2%f} MB, peak memory: {:2.2%f} MB".format(
+        print("<{}>: current memory allocated: {:2.3f} MB, peak memory: {:2.3f} MB".format(
             info, torch.cuda.memory_allocated(device)/1048576, torch.cuda.max_memory_allocated(device)/1048576))
 
