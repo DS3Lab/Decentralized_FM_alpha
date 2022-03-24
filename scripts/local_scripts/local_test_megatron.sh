0d8a620de5adb478e7aff755c660eb49d10744ad
binhang <yuanbinhang@hotmail.com>
2022-03-24 09:30:28 +0100

    foo update

--- scripts/local_scripts/local_test_megatron.sh
+++ scripts/local_scripts/local_test_megatron.sh
@@ -8,14 +8,15 @@ NNODES=1
 NODE_RANK=0
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 
-DATA_PATH=./glue_dataset/data/QQP/
-VOCAB_FILE=./glue_dataset/data/bert-large-cased-vocab.txt
-CHECKPOINT_PATH=./
+TRAIN_DATA=data/glue_dataset/data/QQP/train.tsv
+VALID_DATA=data/glue_dataset/data/QQP/test.tsv
+VOCAB_FILE=bert-vocab.txt
 
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
 python -m torch.distributed.launch $DISTRIBUTED_ARGS \
-       ../Megatron-LM/pretrain_bert.py \
+       ../Megatron-LM/tasks/main.py \
+       --task QQP \
        --tensor-model-parallel-size 2 \
        --pipeline-model-parallel-size 2 \
        --num-layers 24 \
@@ -25,12 +26,10 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --global-batch-size 16 \
        --seq-length 512 \
        --max-position-embeddings 512 \
-       --train-iters 1000000 \
-       --save $CHECKPOINT_PATH \
-       --load $CHECKPOINT_PATH \
-       --data-path $DATA_PATH \
+       --train-iters 50 \
+       --train-data $TRAIN_DATA \
+       --valid-data $VALID_DATA \
        --vocab-file $VOCAB_FILE \
-       --data-impl mmap \
        --split 949,50,1 \
        --distributed-backend nccl \
        --lr 0.0001 \
@@ -40,8 +39,6 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --weight-decay 1e-2 \
        --clip-grad 1.0 \
        --lr-warmup-fraction .01 \
-       --log-interval 100 \
-       --save-interval 10000 \
+       --log-interval 1 \
        --eval-interval 1000 \
-       --eval-iters 10 \
-       --fp16
+       --eval-iters 1
