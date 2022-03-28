81da4a6a9f77aaf0071014790d5cbb39a35f41d3
binhang <yuanbinhang@hotmail.com>
2022-03-28 23:38:02 +0200

    foo

--- scripts/local_scripts/local_test_multi_GPU_megatron_QQP.sh
+++ scripts/local_scripts/local_test_multi_GPU_megatron_QQP.sh
@@ -52,5 +52,5 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --train-iters 10 \
        --no-bias-dropout-fusion \
        --checkpoint-activations \
-       --DDP-impl local \
+       --DDP-impl torch \
        --fp16
