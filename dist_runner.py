c5814509061854cc36c5399a230d91c2cb8d5138
binhang <yuanbinhang@hotmail.com>
2022-04-07 14:21:17 +0200

    foo

--- dist_runner.py
+++ dist_runner.py
@@ -61,7 +61,7 @@ def main():
     else:
         trace_file = './trace_json/gpt3_' + args.pp_mode + '_' + args.dp_mode + get_learning_arguments_str(args) \
                      + get_model_arguments_str(args) + get_dist_arguments_str(args) + '_' + \
-                     get_mixed_precision_arguments_str() + args.profiling + '_' + args.trace_postfix + '.json'
+                     get_mixed_precision_arguments_str(args) + args.profiling + '_' + args.trace_postfix + '.json'
         if args.profiling == 'tidy_profiling':
             distributed_train_foo_iter(args, pipe, device, train_data_loader)
             pipe.export_profiling_result(filename=trace_file)
