15278ecee99bbbf16492c54cdd2712dbeb4b82f6
binhang <yuanbinhang@hotmail.com>
2022-04-25 16:56:20 +0200

    Get a tiny example works for offload.

--- unit_tests/macro_benchmark_tensor_copy.py
+++ unit_tests/macro_benchmark_tensor_copy.py
@@ -5,9 +5,8 @@ import numpy
 
 
 def copy_tensor_torch(n:int, dim:int, test_upload:bool, test_offload:bool):
-    tensors_cpu = [torch.mul(torch.eye(dim, dtype=torch.float32, device='cpu'), 1 + 0.0005 * float(i)) for i in range(n)]
-    for tensor in tensors_cpu:
-        tensor = tensor.pin_memory()
+    tensors_cpu = [torch.mul(torch.eye(dim, dtype=torch.float32, device='cpu'), 1 + 0.01 * float(i)).pin_memory() for i in range(n)]
+
     input_tensors_gpu = [torch.eye(dim, dtype=torch.float16, device='cuda:0') * i for i in range(n)]
     output_tensors_gpu = [torch.zeros((dim, dim), dtype=torch.float16, device='cuda:0') * i for i in range(n)]
 
@@ -37,7 +36,7 @@ def copy_tensor_torch(n:int, dim:int, test_upload:bool, test_offload:bool):
             with torch.cuda.stream(comp_stream):
                 comp_stream.wait_event(copy_to_gpu_ready_events[i])
                 comp_stream.record_event(gpu_compute_start_events[i])
-                output_tensors_gpu[i] = torch.matrix_power(input_tensors_gpu[i], 20)
+                output_tensors_gpu[i] = torch.matrix_power(input_tensors_gpu[i], 4)
                 comp_stream.record_event(gpu_compute_ready_events[i])
             if test_offload:
                 with torch.cuda.stream(gpu_to_cpu_stream):
@@ -90,12 +89,14 @@ def copy_tensor_torch(n:int, dim:int, test_upload:bool, test_offload:bool):
 
 
 # This is from the original cupy lib.
+def _pin_memory(array):
+    mem = cupy.cuda.alloc_pinned_memory(array.nbytes)
+    ret = numpy.frombuffer(mem, array.dtype, array.size).reshape(array.shape)
+    ret[...] = array
+    return ret
+
+
 def demo_cupy():
-    def _pin_memory(array):
-        mem = cupy.cuda.alloc_pinned_memory(array.nbytes)
-        ret = numpy.frombuffer(mem, array.dtype, array.size).reshape(array.shape)
-        ret[...] = array
-        return ret
     SIZE = 1024 * 1024
     x_cpu_src = numpy.arange(SIZE, dtype=numpy.float32)
     x_gpu_src = cupy.arange(SIZE, dtype=numpy.float32)
@@ -115,16 +116,16 @@ def demo_cupy():
     x_gpu_dst = cupy.empty(x_cpu_src.shape, x_cpu_src.dtype)
     x_cpu_dst = numpy.empty(x_gpu_src.shape, x_gpu_src.dtype)
 
-    # x_pinned_cpu_src = _pin_memory(x_cpu_src)
-    # x_pinned_cpu_dst = _pin_memory(x_cpu_dst)
+    x_pinned_cpu_src = _pin_memory(x_cpu_src)
+    x_pinned_cpu_dst = _pin_memory(x_cpu_dst)
 
     with cupy.cuda.stream.Stream() as stream_htod:
         start = stream_htod.record()
-        # x_gpu_dst.set(x_pinned_cpu_src)
-        x_gpu_dst.set(x_cpu_src)
+        x_gpu_dst.set(x_pinned_cpu_src)
+        # x_gpu_dst.set(x_cpu_src) # This does not work for async! Pin memory is necessary!
     with cupy.cuda.stream.Stream() as stream_dtoh:
-        # x_gpu_src.get(out=x_pinned_cpu_dst)
-        x_cpu_dst = x_gpu_src.get()
+        x_gpu_src.get(out=x_pinned_cpu_dst)
+        # x_cpu_dst = x_gpu_src.get() # This does not work for async! Pin memory is necessary!
 
     end = stream_htod.record()
     stream_dtoh.synchronize()
@@ -134,6 +135,93 @@ def demo_cupy():
     print(cupy.cuda.get_elapsed_time(start, end))
 
 
-# copy_tensor_torch(10, 4096, True, True)
+def copy_tensor_torch_cupy(n: int, dim: int):
+    input_pin_numpy_cpu = [_pin_memory(numpy.eye(dim, dtype=numpy.float16) * (1 + 0.01 * float(i))) for i in range(n)]
+    output_pin_numpy_cpu = [_pin_memory(numpy.zeros((dim, dim), dtype=numpy.float16)) for _ in range(n)]
 
+    input_tensors_torch_gpu = [torch.zeros((dim, dim), dtype=torch.float16, device='cuda:0') for _ in range(n)]
+    input_tensors_cupy_gpu = [cupy.asarray(input_tensors_torch_gpu[i]) for i in range(n)]
+    output_tensors_torch_gpu = [torch.zeros((dim, dim), dtype=torch.float16, device='cuda:0') for _ in range(n)]
+    output_tensors_cupy_gpu = [cupy.asarray(output_tensors_torch_gpu[i]) for i in range(n)]
+
+    torch_cpu_to_gpu_stream = torch.cuda.Stream(device='cuda:0', priority=-1)
+    cupy_cpu_to_gpu_stream = cupy.cuda.ExternalStream(torch_cpu_to_gpu_stream.cuda_stream)
+    torch_gpu_to_cpu_stream = torch.cuda.Stream(device='cuda:0', priority=-1)
+    cupy_gpu_to_cpu_stream = cupy.cuda.ExternalStream(torch_gpu_to_cpu_stream.cuda_stream)
+    comp_stream = torch.cuda.default_stream(device='cuda:0')
+
+    copy_to_gpu_start_events = [torch.cuda.Event(enable_timing=True, blocking=False) for _ in range(n)]
+    copy_to_gpu_ready_events = [torch.cuda.Event(enable_timing=True, blocking=False) for _ in range(n)]
+    gpu_compute_start_events = [torch.cuda.Event(enable_timing=True, blocking=False) for _ in range(n)]
+    gpu_compute_ready_events = [torch.cuda.Event(enable_timing=True, blocking=False) for _ in range(n)]
+    copy_to_cpu_start_events = [torch.cuda.Event(enable_timing=True, blocking=False) for _ in range(n)]
+    copy_to_cpu_ready_events = [torch.cuda.Event(enable_timing=True, blocking=False) for _ in range(n)]
+
+    start_event = torch.cuda.Event(enable_timing=True, blocking=False)
+    end_event = torch.cuda.Event(enable_timing=True, blocking=False)
+
+    for _ in range(3):
+        torch.cuda.synchronize()
+        start_event.record()
+        for i in range(n):
+            with torch.cuda.stream(torch_cpu_to_gpu_stream):
+                torch_cpu_to_gpu_stream.record_event(copy_to_gpu_start_events[i])
+                with cupy_cpu_to_gpu_stream:
+                    input_tensors_cupy_gpu[i].set(input_pin_numpy_cpu[i])
+                    # cupy_cpu_to_gpu_stream.synchronize()
+                torch_cpu_to_gpu_stream.record_event(copy_to_gpu_ready_events[i])
+            with torch.cuda.stream(comp_stream):
+                comp_stream.wait_event(copy_to_gpu_ready_events[i])
+                comp_stream.record_event(gpu_compute_start_events[i])
+                torch.matrix_power(input=input_tensors_torch_gpu[i], n=4, out=output_tensors_torch_gpu[i])
+                comp_stream.record_event(gpu_compute_ready_events[i])
+            with torch.cuda.stream(torch_gpu_to_cpu_stream):
+                torch_gpu_to_cpu_stream.wait_event(gpu_compute_ready_events[i])
+                torch_gpu_to_cpu_stream.record_event(copy_to_cpu_start_events[i])
+                with cupy_gpu_to_cpu_stream:
+                    output_tensors_cupy_gpu[i].get(out=output_pin_numpy_cpu[i])
+                    # cupy_gpu_to_cpu_stream.synchronize()
+                torch_gpu_to_cpu_stream.record_event(copy_to_cpu_ready_events[i])
+        end_event.record()
+        torch.cuda.synchronize()
+
+    print("Total time: ", start_event.elapsed_time(end_event))
+
+    for i in range(n):
+        print("===============================", i, "===============================")
+        print("input_numpy_cpu:", input_pin_numpy_cpu[i])
+        print("Input torch gpu:", input_tensors_torch_gpu[i])
+        print("Input cupy gpu:", input_tensors_cupy_gpu[i])
+        print("Output torch gpu:", output_tensors_torch_gpu[i])
+        print("Output cupy gpu:", output_tensors_cupy_gpu[i])
+        print("Output numpy cpu:", output_pin_numpy_cpu[i])
+
+    profile_logs = []
+
+    for i in range(n):
+        copy_to_gpu_ts = start_event.elapsed_time(copy_to_gpu_start_events[i]) * 1e+3
+        copy_to_gpu_slot = copy_to_gpu_start_events[i].elapsed_time(copy_to_gpu_ready_events[i]) * 1e+3
+        copy_to_gpu_log = {"name": "to_gpu", "ph": "X", "pid": 0, "tid": "1. Copy to GPU", "ts": copy_to_gpu_ts,
+                           "dur": copy_to_gpu_slot, "args": {"micro-batch": i}, "cname": "startup"}
+        profile_logs.append(copy_to_gpu_log)
+
+        gpu_compute_ts = start_event.elapsed_time(gpu_compute_start_events[i]) * 1e+3
+        gpu_compute_slot = gpu_compute_start_events[i].elapsed_time(gpu_compute_ready_events[i]) * 1e+3
+        gpu_compute_log = {"name": "matrix_power", "ph": "X", "pid": 0, "tid": "2. GPU Compute", "ts": gpu_compute_ts,
+                           "dur": gpu_compute_slot, "args": {"micro-batch": i}, "cname": "good"}
+        profile_logs.append(gpu_compute_log)
+
+        copy_to_cpu_ts = start_event.elapsed_time(copy_to_cpu_start_events[i]) * 1e+3
+        copy_to_cpu_slot = copy_to_cpu_start_events[i].elapsed_time(copy_to_cpu_ready_events[i]) * 1e+3
+        copy_to_cpu_log = {"name": "to_cpu", "ph": "X", "pid": 0, "tid": "3. Copy to CPU", "ts": copy_to_cpu_ts,
+                           "dur": copy_to_cpu_slot, "args": {"micro-batch": i}, "cname": "thread_state_iowait"}
+        profile_logs.append(copy_to_cpu_log)
+
+    path = '../trace_json/local_debug_CPU_GPU_torch_cupy_copy_both.json'
+    with open(path, 'w') as outfile:
+        json.dump(profile_logs, outfile)
+
+
+copy_tensor_torch(10, 2048, True, True)
 # demo_cupy()
+copy_tensor_torch_cupy(10, 2048)
