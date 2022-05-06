919d04bd3c99396a28dc49ee0af95f10d6a254ba
binhang <yuanbinhang@hotmail.com>
2022-05-06 12:58:19 +0200

    updated heter3

--- scripts/aws_run_gpt3_1gpu_training.sh
+++ scripts/aws_run_gpt3_1gpu_training.sh
@@ -19,13 +19,13 @@ do
     ssh -i ../binhang_ds3_aws_oregon.pem ubuntu@"${ips[rank]}" "bash -s" < ./local_scripts/"${script}" "$master_ip" "$world_size" "$rank" 0 "$ga_step" "$num_layers" "$batch_size" "$log_mode"&
   elif [ $# -eq 5 ]
   then
-    case=$2
+    case=$5
     echo "Running in heterogeneous network: Case-$case"
     ssh -i ../binhang_ds3_aws_oregon.pem ubuntu@"${ips[rank]}" "bash -s" < ./local_scripts/"${script}" "$master_ip" "$world_size" "$rank" 0 "$ga_step" "$num_layers" "$batch_size" "$log_mode" "$case" &
   elif [ $# -eq 6 ]
   then
-    delay_ms=$2
-    rate_gbit=$3
+    delay_ms=$5
+    rate_gbit=$6
     ssh -i ../binhang_ds3_aws_oregon.pem ubuntu@"${ips[rank]}" "bash -s" < ./local_scripts/"${script}" "$master_ip" "$world_size" "$rank" 0 "$ga_step" "$num_layers" "$batch_size" "$log_mode" "$delay_ms" "$rate_gbit" &
   else
     echo "Error! Not valid arguments."
