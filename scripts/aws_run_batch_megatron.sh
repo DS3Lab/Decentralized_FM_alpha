307ac0cbf9af54cb210ebf4adfa67faef7e120a9
binhang <yuanbinhang@hotmail.com>
2022-05-06 23:02:33 +0200

    add some results.

--- scripts/aws_run_batch_megatron.sh
+++ scripts/aws_run_batch_megatron.sh
@@ -2,14 +2,23 @@ case=$1
 batches=(1024 2048 4096)
 layers=(24 32 40)
 
-for batch in "${batches[@]}"
+for layer in "${layers[@]}"
 do
-  for layer in "${layers[@]}"
+  for batch in "${batches[@]}"
   do
     echo "$batch, $layer"
     bash aws_run_megatron_training.sh megatron_gpt3_xl_mp8_dp8.sh 8 1 1 "$layer" "$batch" "$case"
     sleep 10s
   done
 done
+
+# bash aws_run_megatron_training.sh megatron_gpt3_xl_mp8_dp8.sh 1 8 1 24 1024 "$case"
+# sleep 10s
+
+# bash aws_run_megatron_training.sh megatron_gpt3_xl_mp8_dp8.sh 4 2 1 24 1024 "$case"
+# sleep 10s
+
+# bash aws_run_megatron_training.sh megatron_gpt3_xl_mp8_dp8.sh 2 4 1 24 1024 "$case"
+# sleep 10s
 
 bash copy_rank_last_logs.sh
--- scripts/aws_run_megatron_training.sh
+++ scripts/aws_run_megatron_training.sh
@@ -11,13 +11,9 @@ GPUS_PER_NODE=$4
 num_layers=$5
 global_batch_size=$6
 
-rank_map=(0 2 32 33 4 10 7 45 36 8 51 26 11 5 53 1 40 23 37 14 13 43 54 21 57 35 63 18 6 24 16 22 38 3 58 61 44 27 52 30 15 9 39 47 48 41 31 20 12 28 34 42 17 55 19 25 56 60 59 50 49 46 29 62)
-
-
-for i in "${!ips[@]}"
+for rank in "${!ips[@]}"
 do
-  rank=${rank_map[$i]}
-  ip=${ips[i]}
+  ip=${ips[rank]}
   echo "Issue command $script in Rank-$rank node: ${ips[$rank]}"
   if [ $rank -eq 63 ]
   then
