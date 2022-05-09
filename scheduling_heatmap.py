fdc631f477ddb0aa0ab76d7a3b09c39150a9ae5f
Yongjun He <yongjunh@staff-net-als-0679.intern.ethz.ch>
2022-05-09 11:04:27 +0200

    change scale of y axis

--- scheduling_heatmap.py
+++ scheduling_heatmap.py
@@ -19,24 +19,12 @@ sns.set_theme()
 
 for i, simulate_case in enumerate(simulate_cases):
     peer_delay, peer_bandwidth, regions = simulate_case()
-    if i <= 1:
-        cur_vmax = 1
-    elif i <= 3:
-        cur_vmax = 20
-    else:
-        cur_vmax = 300
     ax = sns.heatmap(ax=axes[i, 0], data=pd.DataFrame(peer_delay, index=list(range(config.nodes)), columns=list(range(
-        config.nodes))), vmin=0, vmax=cur_vmax, xticklabels=8, yticklabels=8, linewidths=0.001, square=True, cbar_kws={'label': 'Delay(ms)'})
+        config.nodes))), vmin=0, vmax=max(1, np.amax(peer_delay)), xticklabels=8, yticklabels=8, linewidths=0.001, square=True, cbar_kws={'label': 'Delay(ms)'})
     ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
 
-    if i <= 1:
-        cur_vmax = 100
-    elif i == 2:
-        cur_vmax = 10
-    else:
-        cur_vmax = 2
-    ax = sns.heatmap(ax=axes[i, 1], data=pd.DataFrame(peer_bandwidth, index=list(range(config.nodes)), columns=list(range(config.nodes))), vmin=0, vmax=cur_vmax, xticklabels=8, yticklabels=8, linewidths=0.001, square=True, cbar_kws={'label': 'Bandwidth(Gbps)'})
-
+    ax = sns.heatmap(ax=axes[i, 1], data=pd.DataFrame(peer_bandwidth, index=list(range(config.nodes)), columns=list(range(
+        config.nodes))), vmin=0, vmax=np.amax(peer_bandwidth), xticklabels=8, yticklabels=8, linewidths=0.001, square=True, cbar_kws={'label': 'Bandwidth(Gbps)'})
 
 fig.supxlabel("Node IDs")
 fig.supylabel("Node IDs")
