79d73e8547a39173277ca66ef04d74664977502a
lorrin <zjuwangjue@gmail.com>
2022-05-13 14:02:56 +0200

    add gpipe sync

--- pipeline_parallel/dist_pp_utils.py
+++ pipeline_parallel/dist_pp_utils.py
@@ -1,4 +1,5 @@
 from .dist_gpipe_pipeline_async import GpipeAsync
+from .dist_gpipe_pipeline_sync import GpipeSync
 from .dist_1f1b_pipeline_async import Pipe1F1BAsync
 from modules.dist_deberta_pp_module import *
 
@@ -6,6 +7,8 @@ from modules.dist_deberta_pp_module import *
 def get_pp_module(args, config, device, use_dp):
     if args.pp_mode == 'gpipe':
         return GpipeAsync(args, config, device, use_dp)
+    elif args.pp_mode == 'gpipe_sync':
+        return GpipeSync(args, config, device, use_dp)
     elif args.pp_mode == '1f1b':
         raise Exception('not implemented')
         return Pipe1F1BAsync(args, config, device, use_dp)
@@ -16,6 +19,13 @@ def get_pp_module(args, config, device, use_dp):
 def get_deberta_pp_module(args, config, device, use_dp):
     if args.pp_mode == 'gpipe':
         return GpipeAsync(
+            args, config, device, use_dp,
+            _StageFirst=DebertaStageFirst,
+            _StageLast=DebertaStageLast,
+            _StageMiddle=DebertaStageMiddle,
+        )
+    elif args.pp_mode == 'gpipe_sync':
+        return GpipeSync(
             args, config, device, use_dp,
             _StageFirst=DebertaStageFirst,
             _StageLast=DebertaStageLast,
