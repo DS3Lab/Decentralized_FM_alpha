76765e9290b4c1934eb6edf0a01992a4c473ce18
lorrin <zjuwangjue@gmail.com>
2022-05-13 16:46:45 +0200

    update

--- pipeline_parallel/dist_gpipe_pipeline_sync.py
+++ pipeline_parallel/dist_gpipe_pipeline_sync.py
@@ -97,8 +97,8 @@ class GpipeSync:
         self.enable_tidy_profiling = (args.profiling == 'tidy_profiling')
         self.device = device
         self.torch_comp_stream = torch.cuda.default_stream(device=device)
-        self.torch_recv_stream = self.torch_comp_stream #torch.cuda.Stream(device=device, priority=-1)
-        self.torch_send_stream = self.torch_comp_stream #torch.cuda.Stream(device=device, priority=-1)
+        self.torch_recv_stream = torch.cuda.Stream(device=device, priority=-1)
+        self.torch_send_stream = torch.cuda.Stream(device=device, priority=-1)
 
         self.forward_recv_ready_events = [torch.cuda.Event(enable_timing=self.enable_tidy_profiling, blocking=False)
                                           for _ in range(self.micro_batch_num)]
