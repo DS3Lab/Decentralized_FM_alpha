89a0929ac83d59e3236c15dd4a8574d9af34646e
Jue <zjuwangjue@gmail.com>
2022-05-16 17:44:09 +0000

    update

--- tasks/data_loaders/cola.py
+++ tasks/data_loaders/cola.py
@@ -1,7 +1,8 @@
 
 import os
 import torch
-from datasets import load_dataset, load_from_disk
+from datasets import load_dataset, load_from_disk, Dataset
+from comm.comm_utils import *
 
 def get_cola_data_loader(args, tokenizer, data_split='train', num_workers=0):
     
@@ -14,6 +15,20 @@ def get_cola_data_loader(args, tokenizer, data_split='train', num_workers=0):
         train_set = load_from_disk('./data/glue_cola')[data_split]
     else:
         train_set = load_dataset('glue', 'cola', split=data_split)
+    
+    use_dp = (args.world_size != args.pipeline_group_size)
+    if data_split == 'train' and use_dp:
+        dp_rank = get_data_parallel_rank()
+        n_samples = len(train_set)
+        n_samples_per_rank = n_samples // args.data_group_size
+        i_begin, i_end = dp_rank * n_samples_per_rank, (dp_rank+1) * n_samples_per_rank
+        train_set = Dataset.from_dict(train_set[i_begin: i_end])
+        new_column = list(range(len(train_set)))
+        train_set = train_set.remove_columns(['idx'])
+        train_set = train_set.add_column('idx', new_column)
+    else:
+        dp_rank = 0
+        
     train_set = train_set.map(_encode, batched=True)
     train_set = train_set.map(lambda examples: {'text': examples['input_ids']}, batched=True)
     if 'token_type_ids' in train_set.features:
@@ -29,7 +44,7 @@ def get_cola_data_loader(args, tokenizer, data_split='train', num_workers=0):
     
     if data_split == 'train':
         generator = torch.Generator()
-        generator.manual_seed(args.seed)
+        generator.manual_seed(args.seed+dp_rank)
         train_sampler = torch.utils.data.RandomSampler(train_set, generator=generator)
         train_data_loader = torch.utils.data.DataLoader(train_set,
                                                         batch_size=args.batch_size,
