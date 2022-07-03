8e0cea738ae32f9afc508a7e67960f00227079f2
binhang <yuanbinhang@hotmail.com>
2022-07-03 15:41:11 +0200

    try lsf

--- foo.py
+++ foo.py
@@ -2,3 +2,6 @@ import torch
 import cupy
 
 print("<== Load torch and cupy. ==>")
+print("Torch Version: ", torch.__version__)
+print("CuPy Version: ", cupy.__version__)
+print('GPU available: ', torch.cuda.is_available())
