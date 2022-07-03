ebddb6e94ad211f75271fbe7fa273f7c988e5de1
Yongjun He <yongjunh@Yongjuns-MacBook-Pro.local>
2022-07-03 12:06:15 +0200

    retains 6 digits after the decimal point

--- convergence_lineplot.py
+++ convergence_lineplot.py
@@ -8,30 +8,31 @@ fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(10, 5))
 plt.subplots_adjust(wspace=0.25, hspace=0.55)
 
 for case_idx in range(5):
-    random_scheduler_results = np.zeros(shape=(5000))
-    hybrid_scheduler_results = np.zeros(shape=(5000))
-    our_scheduler_results = np.zeros(shape=(5000))
+    iterations = 15000 if case_idx == 1 else 1500
+    random_scheduler_results = np.zeros(shape=(iterations))
+    hybrid_scheduler_results = np.zeros(shape=(iterations))
+    our_scheduler_results = np.zeros(shape=(iterations))
     for repetition in range(3):
         with open('data/random_scheduler_' + str(case_idx) + '_' + str(repetition) + '.npy', 'rb') as f:
-            random_scheduler_results += np.load(f) / 3
+            random_scheduler_results += np.load(f)[:iterations] / 3
 
         with open('data/hybrid_scheduler_' + str(case_idx) + '_' + str(repetition) + '.npy', 'rb') as f:
-            hybrid_scheduler_results += np.load(f) / 3
+            hybrid_scheduler_results += np.load(f)[:iterations] / 3
 
         with open('data/our_scheduler_' + str(case_idx) + '_' + str(repetition) + '.npy', 'rb') as f:
-            our_scheduler_results += np.load(f) / 3
+            our_scheduler_results += np.load(f)[:iterations] / 3
 
     assert(len(random_scheduler_results) == len(hybrid_scheduler_results))
     assert(len(random_scheduler_results) == len(our_scheduler_results))
 
-    data=[]
-    for i in range(5000):
+    data = []
+    for i in range(iterations):
         data.append([i, random_scheduler_results[i], 'Random'])
         data.append([i, hybrid_scheduler_results[i], 'Hybrid'])
         data.append([i, our_scheduler_results[i], 'Ours'])
 
-    df=pd.DataFrame(data, columns=['trial', 'cost', 'scheduler'])
-    ax=sns.lineplot(ax=axes[case_idx // 3, case_idx % 3], hue_order=['Ours', 'Hybrid', 'Random'], linewidth=2,
+    df = pd.DataFrame(data, columns=['trial', 'cost', 'scheduler'])
+    ax = sns.lineplot(ax=axes[case_idx // 3, case_idx % 3], hue_order=['Ours', 'Hybrid', 'Random'], linewidth=2,
                       palette=['tab:green', 'tab:orange', 'tab:blue'],
                       data=df, x="trial", y="cost", hue='scheduler')
     ax.lines[2].set_linestyle("--")
@@ -39,35 +40,39 @@ for case_idx in range(5):
     ax.set_xlabel(None)
     ax.set_ylabel(None)
     if case_idx == 0:
-        ax.set(ylim=(0, 20))
-        ax.set_yticks([0, 5, 10, 15, 20])
-        ax.set_yticklabels([0, 5, 10, 15, 20], fontsize=15)
+        ax.set(ylim=(0, 25))
+        ax.set_yticks([0, 5, 10, 15, 20, 25])
+        ax.set_yticklabels([0, 5, 10, 15, 20, 25], fontsize=15)
     elif case_idx == 1:
-        ax.set(ylim=(0, 40))
-        ax.set_yticks([0, 10, 20, 30, 40])
-        ax.set_yticklabels([0, 10, 20, 30, 40], fontsize=15)
+        ax.set(ylim=(0, 80))
+        ax.set_yticks([0, 20, 40, 60, 80])
+        ax.set_yticklabels([0, 20, 40, 60, 80], fontsize=15)
     elif case_idx == 2:
-        ax.set(ylim=(0, 40))
-        ax.set_yticks([0, 10, 20, 30, 40])
-        ax.set_yticklabels([00, 10, 20, 30, 40], fontsize=15)
+        ax.set(ylim=(0, 80))
+        ax.set_yticks([0, 20, 40, 60, 80])
+        ax.set_yticklabels([0, 20, 40, 60, 80], fontsize=15)
     elif case_idx == 3:
-        ax.set(ylim=(0, 60))
-        ax.set_yticks([0, 20, 40, 60])
-        ax.set_yticklabels([0, 20, 40, 60], fontsize=15)
+        ax.set(ylim=(0, 80))
+        ax.set_yticks([0, 20, 40, 60, 80])
+        ax.set_yticklabels([0, 20, 40, 60, 80], fontsize=15)
     elif case_idx == 4:
         ax.set(ylim=(0, 200))
         ax.set_yticks([0, 50, 100, 150, 200])
         ax.set_yticklabels([0, 50, 100, 150, 200], fontsize=15)
 
-    ax.set_xticks([0, 2500, 5000])
-    ax.set_xticklabels([0, 2500, 5000], fontsize=15)
+    if case_idx == 1:
+        ax.set_xticks([0, 5000, 10000, 15000])
+        ax.set_xticklabels([0, 5000, 10000, 15000], fontsize=15)
+    else:
+        ax.set_xticks([0, 500, 1000, 1500])
+        ax.set_xticklabels([0, 500, 1000, 1500], fontsize=15)
 
     if case_idx == 4:
         ax.get_legend().set_title(None)
-        handles, labels=ax.get_legend_handles_labels()
+        handles, labels = ax.get_legend_handles_labels()
         handles[2].set_linestyle('--')
-        handles=[handles[2], handles[1], handles[0]]
-        labels=[labels[2], labels[1], labels[0]]
+        handles = [handles[2], handles[1], handles[0]]
+        labels = [labels[2], labels[1], labels[0]]
         ax.legend(handles, labels, ncol=1, handletextpad=0.3,
                   loc='upper left', bbox_to_anchor=(1.2, 1), fontsize=15)
     else:
--- scheduler.py
+++ scheduler.py
@@ -189,7 +189,7 @@ def compute_pipeline_parallel_cost(candidate_partition=None):
     return dp_pipeline_parallel_cost, dp_pipeline_parallel_path, dp_pipeline_parallel_match
 
 
-def GCMA(nodes=None, population_size=None, trails=None):
+def GCMA(nodes=None, population_size=None, trails=None, mode=None):
     # https://dl.acm.org/doi/10.5555/2933718.2933740
     def five_point_crossover(parent1=None, parent2=None):
         parent1_str = [0] * num_devices
@@ -219,7 +219,7 @@ def GCMA(nodes=None, population_size=None, trails=None):
         return parent2_str
 
     def cyclic_partitioning(offspring=None):
-        def calculate_gain(cur_offspring=None, locked_v_idx=None):
+        def calculate_gain_default(cur_offspring=None, locked_v_idx=None):
             partition_sizes = [0] * way
             for partition_idx in cur_offspring:
                 partition_sizes[partition_idx] += 1
@@ -254,6 +254,40 @@ def GCMA(nodes=None, population_size=None, trails=None):
                 if v_idx != None:
                     for target_partition_idx, target_gain in enumerate(gain[v_idx]):
                         if target_partition_idx != partition_idx:
+                            target_gain = round(
+                                target_gain, 6) - round(gain[v_idx][partition_idx], 6)
+                            if target_gain > G_ij[partition_idx, target_partition_idx]:
+                                G_ij[partition_idx,
+                                     target_partition_idx] = target_gain
+                            if target_gain > G_i[partition_idx]:
+                                G_i[partition_idx] = target_gain
+                                G_i_trace[partition_idx] = [
+                                    v_idx, target_partition_idx]
+
+            return G_ij, G_i, G_i_trace
+
+        def calculate_gain_baseline(cur_offspring=None, locked_v_idx=None):
+            gain = np.zeros(shape=(num_devices, way))
+            for v_idx, partition_idx in enumerate(cur_offspring):
+                if locked_v_idx[v_idx] == 0:
+                    for target_idx, target_partition_idx in enumerate(cur_offspring):
+                        partial_pipeline_parallel_cost = peer_delay[v_idx, target_idx] / \
+                            1e3 + send_activation_size * 8 / \
+                            peer_bandwidth[v_idx, target_idx]
+                        partial_data_parallel_cost = peer_delay[v_idx, target_idx] / \
+                            1e3 + send_gradient_size * 8 / \
+                            peer_bandwidth[v_idx, target_idx]
+                        if v_idx != target_idx:
+                            gain[v_idx][target_partition_idx] += partial_pipeline_parallel_cost
+                            gain[v_idx][target_partition_idx] -= partial_data_parallel_cost
+
+            G_i_trace = [[None, None] for i in range(way)]
+            G_i = np.full(shape=(way), fill_value=-np.inf)
+            G_ij = np.full(shape=(way, way), fill_value=-np.inf)
+            for v_idx, partition_idx in enumerate(cur_offspring):
+                if locked_v_idx[v_idx] == 0:
+                    for target_partition_idx, target_gain in enumerate(gain[v_idx]):
+                        if target_partition_idx != partition_idx:
                             target_gain -= gain[v_idx][partition_idx]
                             if target_gain > G_ij[partition_idx, target_partition_idx]:
                                 G_ij[partition_idx,
@@ -275,8 +309,12 @@ def GCMA(nodes=None, population_size=None, trails=None):
                 movements = []
                 epsilon = []
                 tau = []
-                G_ij, G_i, G_i_trace = calculate_gain(
-                    cur_offspring, locked_v_idx)
+                if mode == "default":
+                    G_ij, G_i, G_i_trace = calculate_gain_default(
+                        cur_offspring, locked_v_idx)
+                else:
+                    G_ij, G_i, G_i_trace = calculate_gain_baseline(
+                        cur_offspring, locked_v_idx)
                 S0 = Si = np.argmax(G_i)
                 for _ in range(num_devices):  # how many movement per cycle
                     v_idx, Pv = G_i_trace[Si]
@@ -292,8 +330,12 @@ def GCMA(nodes=None, population_size=None, trails=None):
                     Si = Pv
                     if Si == S0:
                         break
-                    G_ij, G_i, G_i_trace = calculate_gain(
-                        cur_offspring, locked_v_idx)
+                    if mode == "default":
+                        G_ij, G_i, G_i_trace = calculate_gain_default(
+                            cur_offspring, locked_v_idx)
+                    else:
+                        G_ij, G_i, G_i_trace = calculate_gain_baseline(
+                            cur_offspring, locked_v_idx)
 
                 max_sum = 0
                 l = 0
@@ -445,7 +487,7 @@ if __name__ == "__main__":
             #        pipeline_parallel_match = cur_pipeline_parallel_match
 
             candidate_partitions, all_cost_records, min_cost_records = GCMA(
-                nodes=list(range(num_devices)), population_size=100, trails=4900)
+                nodes=list(range(num_devices)), population_size=100, trails=4900, mode="default")
             candidate_partition_idx = np.argmin(all_cost_records)
             candidate_partition = [candidate_partitions[candidate_partition_idx][i: i + partition_size]
                                    for i in range(0, num_devices, partition_size)]
