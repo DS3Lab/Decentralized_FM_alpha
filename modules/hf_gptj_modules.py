7267dea8f3e69eebecdb964b0328a46b9b155dd1
Jue WANG <zjuwangjue@gmail.com>
2022-07-07 18:51:20 +0200

    Create hf_gptj_modules.py

---
+++ modules/hf_gptj_modules.py
@@ -0,0 +1,78 @@
+import torch
+import math
+import numpy as np
+from torch import nn
+from torch.nn import functional
+from torch.utils.checkpoint import checkpoint
+from transformers.modeling_outputs import (
+    BaseModelOutputWithPastAndCrossAttentions,
+    CausalLMOutputWithCrossAttentions,
+)
+from transformers.models.gptj.modeling_gptj import GPTJAttention as _GPTJAttention
+from transformers.models.gptj.modeling_gptj import GPTJMLP as _GPTJMLP
+from transformers.models.gptj.modeling_gptj import GPTJBlock as _GPTJBlock
+from transformers.models.gptj.modeling_gptj import GPTJModel as _GPTJModel
+from transformers.models.gptj.configuration_gptj import GPTJConfig as GPTConfig
+
+
+class GPTEmbeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        
+        self.config = config
+        self.embed_dim = config.hidden_size
+        self.wte = nn.Embedding(config.vocab_size, self.embed_dim)
+        self.drop = nn.Dropout(config.embd_pdrop)
+        
+    def forward(self, input_ids, past_layer=None):
+      
+        device = input_ids.device
+        
+        # input ids
+        input_shape = input_ids.size()
+        input_ids = input_ids.view(-1, input_shape[-1])
+        batch_size = input_ids.shape[0]
+        hidden_states = self.wte(input_ids)
+        hidden_states = self.drop(hidden_states)
+        return hidden_states
+    
+
+class GPTBlock(_GPTJBlock):
+    def __init__(self, config, *args, use_checkpoint=True, **kargs):
+        super().__init__(config=config, *args, **kargs)
+        self.config = config
+        self.use_checkpoint = use_checkpoint
+        
+        def attn_res(x: torch.Tensor, layer_past=None) -> torch.Tensor:
+            res = x
+            x = self.ln_1(x)
+            x, present = self.attn(x, use_cache=True, layer_past=layer_past)
+            return x + res, present
+        self.attn_res = attn_res
+        
+        def mlp_res(x: torch.Tensor) -> torch.Tensor:
+            res = x
+            x = self.ln_2(x)
+            x = self.mlp(x)
+            return x + res
+        self.mlp_res = mlp_res
+
+    def forward(self, x: torch.Tensor, layer_past=None) -> torch.Tensor:
+        
+        x, present = self.attn_res(x, layer_past=layer_past)
+        
+        x = self.mlp_res(x)
+        
+        return x, present
+    
+    
+class GPTLMHead(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_f = nn.LayerNorm(config.n_embd, eps=config.layer_norm_epsilon)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        
+    def forward(self, x, input_ids=None):
+        x = self.ln_f(x)
+        x = self.lm_head(x)
+        return x
