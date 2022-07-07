4e30a42adaee4cff1b6a149caa734ccf26989c79
Ubuntu <ubuntu@ip-172-31-29-139.us-west-2.compute.internal>
2022-07-07 18:51:34 +0000

    update

--- convert_gpt2_checkpoint.py
+++ convert_gpt2_checkpoint.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert HF checkpoints')
     parser.add_argument('--model-name', type=str, default='gpt2', 
                         help='model-name')
-    parser.add_argument('--save-dir', type=str, default='./', 
+    parser.add_argument('--save-dir', type=str, default='./models/', 
                         help='model-name')
     args = parser.parse_args()
     
     if not os.path.exists(args.save_dir):
         os.mkdir(args.save_dir)
-    save_path = os.path.join(args.save_dir, args.model_name)
+    save_path = os.path.join(args.save_dir, args.model_name.replace('/', '_'))
     if not os.path.exists(save_path):
         os.mkdir(save_path)
     
