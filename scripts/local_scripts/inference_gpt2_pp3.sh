b25336c3a46f72f779339d5f99951266eac806c4
binhang <yuanbinhang@hotmail.com>
2022-07-12 15:37:47 +0200

    update some scripts.

--- scripts/local_scripts/inference_gpt2_pp3.sh
+++ scripts/local_scripts/inference_gpt2_pp3.sh
@@ -5,7 +5,7 @@ world_size=$2
 rank=$3
 timestamp=$(date +%Y_%m_%d_%H_%M)
 
-DIST_CONF="--pp-mode gpipe --world-size $world_size --pipeline-group-size $world_size --data-group-size 1 --rank "$rank""
+DIST_CONF="--pp-mode pipe_greedy --world-size $world_size --pipeline-group-size $world_size --data-group-size 1 --rank "$rank""
 INFERENCE_CONF="--batch-size 64 --input-seq-length 128 --generate-seq-length 128 --micro-batch-size 1 --num-layers 4"
 
 
@@ -16,4 +16,4 @@ then
 fi
 
 
-python dist_inference_runner.py --dist-url tcp://"$ip":9000 --fp16 $DIST_CONF $MODEL_CONF>> "./logs/${timestamp}_inference_pp3_default.log"
+python dist_inference_runner.py --dist-url tcp://"$ip":9000 $DIST_CONF $INFERENCE_CONF>> "./logs/${timestamp}_inference_pp3_default.log"
