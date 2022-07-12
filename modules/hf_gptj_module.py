58bd6fd36f0f15dc1357e7e5eff2dd5c457da3ff
Jue Wang <zjuwangjue@gmail.com>
2022-07-12 08:59:52 +0000

    gptj

--- modules/hf_gptj_module.py
+++ modules/hf_gptj_module.py
@@ -1,17 +1,27 @@
-import os
 import torch
+import math
+import numpy as np
 from torch import nn
-from transformers.models.gpt2.modeling_gpt2 import GPT2Block as _GPT2Block
-from transformers.models.gpt2.configuration_gpt2 import GPT2Config as GPTConfig
+from torch.nn import functional
+from torch.utils.checkpoint import checkpoint
+from transformers.modeling_outputs import (
+    BaseModelOutputWithPastAndCrossAttentions,
+    CausalLMOutputWithCrossAttentions,
+)
+from transformers.models.gptj.modeling_gptj import GPTJAttention as _GPTJAttention
+from transformers.models.gptj.modeling_gptj import GPTJMLP as _GPTJMLP
+from transformers.models.gptj.modeling_gptj import GPTJBlock as _GPTJBlock
+from transformers.models.gptj.modeling_gptj import GPTJModel as _GPTJModel
+from transformers.models.gptj.configuration_gptj import GPTJConfig as GPTConfig
 
 
 class GPTEmbeddings(nn.Module):
     def __init__(self, config):
         super().__init__()
+        
         self.config = config
         self.embed_dim = config.hidden_size
         self.wte = nn.Embedding(config.vocab_size, self.embed_dim)
-        self.wpe = nn.Embedding(config.max_position_embeddings, self.embed_dim)
         
     @classmethod
     def from_pretrained(cls, model_path, config=None):
@@ -22,54 +32,21 @@ class GPTEmbeddings(nn.Module):
             model_path, 'pytorch_embs.pt',
         )))
         return module
-
-    def forward(self, input_ids, past_layer=None):
-        if past_layer is not None:
-            past_length = past_layer[0].size(2)
-        else:
-            past_length = 0
-        device = input_ids.device
+        
+    def forward(self, input_ids, *args, **kargs):
+        
         # input ids
         input_shape = input_ids.size()
         input_ids = input_ids.view(-1, input_shape[-1])
-        batch_size = input_ids.shape[0]
-        # position ids
-        position_ids = torch.arange(
-            past_length, past_length + input_shape[-1], dtype=torch.long, device=device)
-        position_ids = position_ids.unsqueeze(0).view(-1, input_shape[-1])
-
-        inputs_embeds = self.wte(input_ids)
-        position_embeds = self.wpe(position_ids)
-        hidden_states = inputs_embeds + position_embeds
-
-        # hidden_states = self.drop(hidden_states)
-
+        hidden_states = self.wte(input_ids)
         return hidden_states
+    
 
-
-class GPTBlock(_GPT2Block):
+class GPTBlock(_GPTJBlock):
     def __init__(self, config, *args, use_checkpoint=True, **kargs):
         super().__init__(config=config, *args, **kargs)
         self.config = config
         self.use_checkpoint = use_checkpoint
-
-        def attn_res(x: torch.Tensor, layer_past=None) -> torch.Tensor:
-            # use_cache = (layer_past is None)
-            use_cache = True
-            res = x
-            x = self.ln_1(x)
-            x, present = self.attn(x, use_cache=use_cache, layer_past=layer_past)
-            return x + res, present
-
-        self.attn_res = attn_res
-
-        def mlp_res(x: torch.Tensor) -> torch.Tensor:
-            res = x
-            x = self.ln_2(x)
-            x = self.mlp(x)
-            return x + res
-
-        self.mlp_res = mlp_res
         
     @classmethod
     def from_pretrained(cls, model_path, config=None, layer_index=None):
@@ -82,17 +59,19 @@ class GPTBlock(_GPT2Block):
         )))
         return module
 
-    def forward(self, x: torch.Tensor, layer_past=None) -> torch.Tensor:
-        x, present = self.attn_res(x, layer_past=layer_past)
-        x = self.mlp_res(x)
-        return x, present
-
-
+    def forward(self, x: torch.Tensor, layer_past=None, attention_mask=None) -> torch.Tensor:
+        res = x
+        x = self.ln_1(x)
+        x_a, present = self.attn(x, use_cache=True, layer_past=layer_past)
+        x_m = self.mlp(x)
+        return x_a + x_m + res, present
+    
+    
 class GPTLMHead(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.ln_f = nn.LayerNorm(config.n_embd, eps=config.layer_norm_epsilon)
-        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size)
         
     @classmethod
     def from_pretrained(cls, model_path, config=None):
@@ -103,9 +82,8 @@ class GPTLMHead(nn.Module):
             model_path, 'pytorch_lm_head.pt',
         )))
         return module
-
-    def forward(self, x, input_ids=None):
+        
+    def forward(self, x):
         x = self.ln_f(x)
         x = self.lm_head(x)
         return x
-
