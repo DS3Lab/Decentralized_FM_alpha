d107310a3f290a6e4a6411f3e9ee8ce5ac23785d
binhang <yuanbinhang@hotmail.com>
2022-07-19 15:51:21 +0200

    foo

--- pipeline_parallel/dist_pipeline_inference_greedy_token_pipe_sync.py
+++ pipeline_parallel/dist_pipeline_inference_greedy_token_pipe_sync.py
@@ -256,7 +256,7 @@ class DistGreedyInferenceTokePipeSync:
             self.output_token_emb[index] = current_emb[:, -1:]
 
     def _forward_compute_generate_token(self, index):
-        print("Compute generate seq micro-batch <", index, ">.")
+        # print("Compute generate seq micro-batch <", index, ">.")
         if self.pp_rank == 0:
             current_emb = self.layers['emb'](self.recv_new_token[index], self.cached_attention[0][index])
         else:
@@ -459,6 +459,7 @@ class DistGreedyInferenceTokePipeSync:
     def forward_new_token_pipeline_stage(self):
         self._merge_cached_seqs_and_attentions()
         for step in range(self.generate_seq_length):
+            print("Compute generate seq step <", step, ">.")
             self.forward_new_token_pipeline_step(step)
 
     def _profile_token_pipeline_step_add_send_slot(self, step: int, i: int):
