d5ed9bd682d9bf9f0e4f9594c13cec52981ae918
binhang <yuanbinhang@hotmail.com>
2022-07-19 17:14:28 +0200

    foo

--- utils/dist_args_utils.py
+++ utils/dist_args_utils.py
@@ -215,6 +215,9 @@ def get_inference_arguments_str(args, add_rank=True, rank=None):
     if args.fp16:
         arg_str += '_fp16'
     arg_str += '_b' + str(args.batch_size) + '_' + str(args.micro_batch_size)
+    if hasattr(args, 'token-micro-batch-size'):
+        arg_str += str(args.token_micro_batch_size)
+
     arg_str += '_s' + str(args.input_seq_length) + '_' + str(args.generate_seq_length)
     arg_str += '_p' + str(args.pipeline_group_size)
     if add_rank:
