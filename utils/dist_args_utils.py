388901b5e61b16d7100495552f1a7559298d88b4
binhang <yuanbinhang@hotmail.com>
2022-07-27 23:10:32 +0200

    test seq hybrid.

--- utils/dist_args_utils.py
+++ utils/dist_args_utils.py
@@ -198,7 +198,13 @@ def add_inference_details_arguments(parser):
                         help='sample from top p')
     parser.add_argument('--temperature', type=float, default=1, metavar='S',
                         help='temperature on logits')
+    parser.add_argument('--token-batch-size', type=int, default=1, metavar='S',
+                        help='token generation micro batch size.')
     parser.add_argument('--token-micro-batch-size', type=int, default=1, metavar='S',
+                        help='token generation micro batch size.')
+    parser.add_argument('--prompt-batch-size', type=int, default=1, metavar='S',
+                        help='token generation micro batch size.')
+    parser.add_argument('--prompt-micro-batch-size', type=int, default=1, metavar='S',
                         help='token generation micro batch size.')
     parser.add_argument('--echo-prompt', type=lambda x: (str(x).lower() == 'true'),
                         default=False, metavar='S',
