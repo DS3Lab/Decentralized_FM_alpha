38130a46d7508fbec1349b027855ac52cc16cd76
binhang <yuanbinhang@hotmail.com>
2022-07-31 18:30:53 +0200

    foo

--- scripts/local_run_deepspeed_batch_on_rank0.sh
+++ scripts/local_run_deepspeed_batch_on_rank0.sh
@@ -8,8 +8,8 @@ for batch in "${batches[@]}"
 do
   for layer in "${layers[@]}"
   do
+    echo "Run tasks of layer ${layer} batch ${batch} .."
     timestamp=$(date +%Y_%m_%d_%H_%M)
     deepspeed --hostfile=./scripts/ds_hostnames dist_deepspeed_zero_s3.py --embedding-dim 2048 --seq-length 2048 --batch-size $batch --num-layers $layer >> "./logs/${timestamp}_deepspeed_L${layer}_B${batch}.log"
   done
 done
-
