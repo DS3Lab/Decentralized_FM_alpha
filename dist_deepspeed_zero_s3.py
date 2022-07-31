0c55b7ac6f33f0c61fe5ec72d23972077bb99e61
binhang <yuanbinhang@hotmail.com>
2022-07-31 21:35:17 +0200

    foo

--- dist_deepspeed_zero_s3.py
+++ dist_deepspeed_zero_s3.py
@@ -49,6 +49,9 @@ def main():
         "zero_optimization": {
             "stage": 3
         },
+        "offload_param": {
+            "device": "cpu"
+        },
         "offload_optimizer": {
             "device": "cpu",
             "buffer_count": 4,
