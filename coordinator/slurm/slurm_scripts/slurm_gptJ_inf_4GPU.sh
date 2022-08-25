701cc7b002b6cfdfc1a94c3933e293ba3673c0d1
Ubuntu <ubuntu@ip-172-31-61-171.us-west-2.compute.internal>
2022-08-25 10:03:08 +0000

    multiple gpus

--- coordinator/slurm/slurm_scripts/slurm_gptJ_inf_4GPU.sh
+++ coordinator/slurm/slurm_scripts/slurm_gptJ_inf_4GPU.sh
@@ -13,22 +13,37 @@ port=$1
 source activate base                          # Activate my conda python environment
 cd /afs/cs.stanford.edu/u/biyuan/GPT-home-private     # Change directory
 
-nvidia-smi
-
-ifconfig
-
-world_size=4
-machine_size=2
-
-DIST_CONF="--pp-mode pipe_sync_greedy --pipeline-group-size $world_size --cuda-id 0"
-MODEL_CONF="--model-type gptj --model-name /sailhome/biyuan/GPT-home-private/pretrained_debug_models/gpt-j-6B --num-iters 10"
-INFERENCE_CONF="--fp16 --batch-size 24 --input-seq-length 512 --generate-seq-length 32 --micro-batch-size 1 --num-layers 7"
-COOR_CONF="--coordinator-server-ip 10.79.12.70  --unique-port $port"
-
 export NCCL_SOCKET_IFNAME=eno1
 export GLOO_SOCKET_IFNAME=eno1
 export NCCL_DEBUG=INFO
 export NCCL_IB_DISABLE=1
 export NCCL_P2P_DISABLE=1
 
-python dist_inference_runner_w_slurm_coordinator.py $DIST_CONF $MODEL_CONF $INFERENCE_CONF $COOR_CONF
+nvidia-smi
+
+ifconfig
+
+world_size=4
+machine_size=2
+n_gpu_per_machine=$((world_size/machine_size))
+
+i=0
+while :
+do
+  if [ "$i" -ge "$n_gpu_per_machine" ]; then
+      break
+  fi
+  
+  DIST_CONF="--pp-mode pipe_sync_greedy --pipeline-group-size $world_size --cuda-id $i"
+  MODEL_CONF="--model-type gptj --model-name /sailhome/biyuan/GPT-home-private/pretrained_debug_models/gpt-j-6B --num-iters 10"
+  INFERENCE_CONF="--fp16 --batch-size 24 --input-seq-length 512 --generate-seq-length 32 --micro-batch-size 1 --num-layers 7"
+  COOR_CONF="--coordinator-server-ip 10.79.12.70  --unique-port $port"
+
+  python dist_inference_runner_w_slurm_coordinator.py $DIST_CONF $MODEL_CONF $INFERENCE_CONF $COOR_CONF &
+
+  ((port++))
+  ((i++))
+
+done
+
+wait
