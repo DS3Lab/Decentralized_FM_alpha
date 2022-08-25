7f8d978044087c948feaab3a819b7661a9b9b86c
Binhang Yuan <biyuan@sc.stanford.edu>
2022-08-25 02:18:59 -0700

    update script

--- coordinator/slurm/slurm_scripts/slurm_gptJ_inf_4GPU.sh
+++ coordinator/slurm/slurm_scripts/slurm_gptJ_inf_4GPU.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=gptJ_inf_4GPU
 #
-#SBATCH --gres gpu:titanx:1
+#SBATCH --partition=jag-standard
+#SBATCH --gres gpu:3090:1
 #SBATCH --time=10:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=4G
 #SBATCH --output=/afs/cs.stanford.edu/u/biyuan/exe_log/gptJ_inf_4GPU_%j.log
 port=$1
 
@@ -19,11 +20,14 @@ ifconfig
 world_size=4
 
 DIST_CONF="--pp-mode pipe_sync_greedy --pipeline-group-size $world_size --cuda-id 0"
-MODEL_CONF="--model-type gptj --model-name ./pretrained_models/gpt-j-6B --num-iters 10"
+MODEL_CONF="--model-type gptj --model-name /sailhome/biyuan/GPT-home-private/pretrained_debug_models/gpt-j-6B --num-iters 10"
 INFERENCE_CONF="--fp16 --batch-size 24 --input-seq-length 512 --generate-seq-length 32 --micro-batch-size 1 --num-layers 7"
 COOR_CONF="--coordinator-server-ip 10.79.12.70  --unique-port $port"
 
-#export NCCL_SOCKET_IFNAME=ens160
-#export GLOO_SOCKET_IFNAME=ens160
+export NCCL_SOCKET_IFNAME=eno1
+export GLOO_SOCKET_IFNAME=eno1
+export NCCL_DEBUG=INFO
+export NCCL_IB_DISABLE=1
+export NCCL_P2P_DISABLE=1
 
 python dist_inference_runner_w_slurm_coordinator.py $DIST_CONF $MODEL_CONF $INFERENCE_CONF $COOR_CONF
