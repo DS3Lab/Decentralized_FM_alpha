c23a0f42f6bce9b71f92b591e88f2e26c06e2bf6
Jue Wang <juewang@eu-login-06.euler.ethz.ch>
2022-09-03 12:42:24 +0200

    revert lsf client

--- coordinator/lsf/lsf_coordinate_client.py
+++ coordinator/lsf/lsf_coordinate_client.py
@@ -44,8 +44,98 @@ class CoordinatorTrainClient:
             msg = s.recv(1024)
             print(f"Received: {msg}")
 
-
 class CoordinatorInferenceClient:
+    def __init__(self, args):
+        self.host_ip = args.coordinator_server_ip
+        self.host_port = args.coordinator_server_port
+        self.client_port = int(args.lsf_job_no) % 10000 + 10000
+
+    def notify_inference_join(self):
+        print("++++++++++++++++++notify_inference_join++++++++++++++++++")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind(('', self.client_port))
+            s.connect((self.host_ip, self.host_port))
+            # s.sendall(b"inference#join#")
+            msg_dict = {
+                'task': 'inference',
+                'state': 'join'
+            }
+            s.sendall(json.dumps(msg_dict).encode())
+            msg = s.recv(1024)
+            print(f"Received: {msg}")
+            msg_arg = client_message_parser(msg, 'join_inference')
+            return msg_arg['prime_ip'], msg_arg['my_rank'], msg_arg['port']
+
+    def notify_inference_finish(self, rank: int, iter_time: float):
+        print("++++++++++++++++++notify_inference_finish++++++++++++++++++")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind(('', self.client_port))
+            s.connect((self.host_ip, self.host_port))
+            # s.sendall(b"inference#finish#"+message.encode())
+            msg_dict = {
+                'task': 'inference',
+                'state': 'finish',
+                'rank': rank,
+                'iter_time': iter_time
+            }
+            s.sendall(json.dumps(msg_dict).encode())
+            msg = s.recv(1024)
+        print(f"Received: {msg}")
+        return msg
+
+    def notify_inference_heartbeat(self):
+        print("++++++++++++++++++notify_inference_heartbeat++++++++++++++++++")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind(('', self.client_port))
+            s.connect((self.host_ip, self.host_port))
+            # s.sendall(b"inference#finish#"+message.encode())
+            msg_dict = {
+                'task': 'inference',
+                'state': 'worker_heartbeats',
+            }
+            s.sendall(json.dumps(msg_dict).encode())
+            msg = s.recv(1024)
+        print(f"Received: {msg}")
+        return msg
+
+    def notify_inference_dequeue_job(self, model_name):
+        print("++++++++++++++++++notify_inference_dequeue_job++++++++++++++++++")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind(('', self.client_port))
+            s.connect((self.host_ip, self.host_port))
+            # s.sendall(b"inference#finish#"+message.encode())
+            msg_dict = {
+                'task': 'inference',
+                'state': 'worker_dequeue',
+                'model_name': model_name
+            }
+            s.sendall(json.dumps(msg_dict).encode())
+            msg = s.recv(8192)
+            return_msg_dict = json.loads(msg)
+        print(f"Received: {return_msg_dict}")
+        return return_msg_dict
+
+    def notify_inference_post_result(self, job_request):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind(('', self.client_port))
+            s.connect((self.host_ip, self.host_port))
+            # s.sendall(b"inference#finish#"+message.encode())
+            msg_dict = {
+                'task': 'inference',
+                'state': 'worker_post_result',
+                'job_request': job_request
+            }
+            s.sendall(json.dumps(msg_dict).encode())
+            msg = s.recv(8192)
+        print(f"Received: {msg}")
+        return msg
+
+class CoordinatorInferenceFolderClient:
     def __init__(self, args, model_name:str):
         self.host_ip = args.coordinator_server_ip
         self.host_port = args.coordinator_server_port
