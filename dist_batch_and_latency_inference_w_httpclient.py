4f801f065d61eebf8934150501fc68ad7cb34e0b
Jue WANG <zjuwangjue@gmail.com>
2022-09-17 00:23:31 +0800

    Update dist_batch_and_latency_inference_w_httpclient.py

--- dist_batch_and_latency_inference_w_httpclient.py
+++ dist_batch_and_latency_inference_w_httpclient.py
@@ -1,4 +1,7 @@
+import os
+from time import sleep
 import argparse
+from loguru import logger
 from pipeline_parallel.dist_pp_utils import get_pp_inference_module
 from utils.dist_args_utils import *
 from utils.dist_inference_utils import *
