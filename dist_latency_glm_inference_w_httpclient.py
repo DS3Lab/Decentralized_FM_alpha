f731997166c29c99a8b9057551a3613f71e056c5
binhang <yuanbinhang@hotmail.com>
2022-10-01 10:48:11 +0200

    foo debug

--- dist_latency_glm_inference_w_httpclient.py
+++ dist_latency_glm_inference_w_httpclient.py
@@ -452,10 +452,10 @@ def main(args):
                     # strategy = BaseStrategy(batch_size=1, temperature=args.temperature, top_k=args.top_k,
                     #                        top_p=args.top_p, end_tokens=end_tokens)
                     # TODO some of our input config is illegal for GLM, need a fix!
-                    strategy = BaseStrategy(batch_size=1, temperature=config['temperature'], top_k=config['top_k'],
+                    strategy = BaseStrategy(batch_size=1, temperature=args.temperature, top_k=config['top_k'],
                                             top_p=args.top_p, end_tokens=end_tokens)
                     # TODO change config to our config, to make it work desired seq length.
-                    answers, answers_with_style, blanks = fill_blanks(raw_text, model, tokenizer, strategy, config=None)
+                    answers, answers_with_style, blanks = fill_blanks(raw_text, model, tokenizer, strategy, config)
                     end_time = time.time()
                     # print(f"Rank-<{dist.get_rank()}>: answer:")
                     # print(answers)
