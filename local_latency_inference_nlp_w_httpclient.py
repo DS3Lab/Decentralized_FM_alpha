20c451a61e1f8d1bfec0b81dab425ca5e41b0bf7
binhang <yuanbinhang@hotmail.com>
2022-10-17 14:29:35 +0200

    add local mode and fixed some issues for small models.

--- local_latency_inference_nlp_w_httpclient.py
+++ local_latency_inference_nlp_w_httpclient.py
@@ -6,6 +6,9 @@ from loguru import logger
 from time import sleep
 from transformers import AutoTokenizer, AutoModelForCausalLM, T5ForConditionalGeneration, AutoModelForSeq2SeqLM
 import torch
+import math
+import numpy as np
+import random
 
 
 def get_huggingface_tokenizer_model(args, device):
@@ -36,15 +39,70 @@ def get_huggingface_tokenizer_model(args, device):
     return tokenizer, model
 
 
-def to_result(output):
-    item = {'choices': [], }
-    choice = {
-        "text": (output[0]),
-        "index": 0,
-        "finish_reason": "length",
-    }
-    item['choices'].append(choice)
-    return item
+def post_processing_text(input_text, output_text, model_name, query):
+    print(f"<post_processing_text> input_text: {input_text}")
+    print(f"<post_processing_text> output_text: {output_text}")
+    stop_tokens = []
+    for token in query.get('stop', []):
+        if token != '':
+            stop_tokens.append(token)
+    print(f"<post_processing_text> stop_tokens: {stop_tokens}.")
+
+    if query.get('max_tokens') == 0:
+        return ""
+
+    if model_name == 'gpt-j-6b' or model_name == 't5-11b':
+        if not query.get('echo', False):
+            text = output_text[len(input_text):]
+        else:
+            text = output_text
+        end_pos = len(text)
+        print(f"<post_processing_text>1 end_pos: {end_pos}.")
+        for stop_token in stop_tokens:
+            if query.get('echo', False):
+                if text[len(input_text):].find(stop_token) != -1:
+                    end_pos = min(text[len(input_text):].find(stop_token) + len(stop_token), end_pos)
+            else:
+                if text.find(stop_token) != -1:
+                    end_pos = min(text.find(stop_token) + len(stop_token), end_pos)
+            print(f"<post_processing_text>2 end_pos: {end_pos}.")
+    elif model_name == 'ul2' or model_name == 't0pp':
+        if query.get('echo', False):
+            text = input_text+' '+output_text
+        else:
+            text = output_text
+        end_pos = len(text)
+        print(f"<post_processing_text>1 end_pos: {end_pos}.")
+        for stop_token in stop_tokens:
+            if query.get('echo', False):
+                if text[len(input_text)+1:].find(stop_token) != -1:
+                    end_pos = min(text[len(input_text)+1:].find(stop_token) + len(stop_token), end_pos)
+            else:
+                if text.find(stop_token) != -1:
+                    end_pos = min(text.find(stop_token) + len(stop_token), end_pos)
+            print(f"<post_processing_text>2 end_pos: {end_pos}.")
+    else:
+        assert False, "Model not supported yet."
+    print(f"<post_processing_text> text: {text}, end_pos: {end_pos}")
+    post_processed_text = text[:end_pos + 1]
+    print(f"<post_processing_text> input: {output_text}")
+    print(f"<post_processing_text> output: {post_processed_text}")
+    return post_processed_text
+
+
+def to_result(input_text, output_text, model_name, query):
+    result = {}
+    items = []
+    for i in range(len(output_text)):
+        item = {'choices': [], }
+        print(f"<to_result> output{i}: {len(input_text[i])} / {len(output_text[i])}")
+        choice = {
+            "text": post_processing_text(input_text[i], output_text[i], model_name, query),
+            "index": 0,
+            "finish_reason": "length"
+        }
+    result['inference_result'] = items
+    return result
 
 
 def main():
@@ -58,6 +116,8 @@ def main():
                         help='trained model path')
     parser.add_argument('--cuda-id', type=int, default=0, metavar='S',
                         help='cuda-id (default:0)')
+    parser.add_argument('--batch-size', type=int, default=8, metavar='S',
+                        help='batch-size for inference (default:8)')
     parser.add_argument('--fp16', action='store_true',
                         help='Run model in fp16 mode.')
     args = parser.parse_args()
@@ -104,9 +164,9 @@ def main():
                         # TODO: we assume len(payload) is 1, right?
                         query = instruction['payload']['payload'][0]
                         if isinstance(query['prompt'], list):
-                            raw_text = query['prompt'][0]
-                        elif isinstance(query['prompt'], str):
                             raw_text = query['prompt']
+                        elif isinstance(query['prompt'], str):
+                            raw_text = [query['prompt']]
                         else:
                             print("wrong prompt format, it can only be str or list of str")
                             print(query['prompt'])
@@ -115,43 +175,51 @@ def main():
                         print(f"Job <{job_id}> has been processed")
 
                         start_time = time.time()
-                        prompt = [raw_text]
 
-                        inputs = tokenizer(
-                            prompt,
-                            padding=True,
-                            truncation=True,
-                            return_tensors="pt",
-                        )
+                        batch_size = min(len(raw_text), args.batch_size)
+                        num_iter = math.ceil(len(raw_text) / batch_size)
+                        answers = []
+                        seed = query.get('seed', None)
+                        if seed is not None:
+                            torch.manual_seed(seed)
+                            np.random.seed(seed)
+                            random.seed(seed)
 
-                        inputs.to(device)
-
-                        if query.get('temperature', 0.9) == 0:
-                            outputs = model.generate(
-                                **inputs, do_sample=True, top_p=query.get('top_p', 0),
-                                temperature=1.0, top_k=1,
-                                max_new_tokens=query.get('max_tokens', 16),
-                                return_dict_in_generate=True,
-                                output_scores=True,  # return logit score
-                                output_hidden_states=True,  # return embeddings
+                        for iter_i in range(num_iter):
+                            current_raw_text = raw_text[iter_i * batch_size: (iter_i + 1) * batch_size]
+                            inputs = tokenizer(
+                                current_raw_text,
+                                padding=True,
+                                truncation=True,
+                                return_tensors="pt",
                             )
-                        else:
-                            outputs = model.generate(
-                                **inputs, do_sample=True, top_p=query.get('top_p', 0),
-                                temperature=query.get('temperature', 0.9),
-                                max_new_tokens=query.get('max_tokens', 16),
-                                return_dict_in_generate=True,
-                                output_scores=True,  # return logit score
-                                output_hidden_states=True,  # return embeddings
-                            )
+                            inputs.to(device)
+                            if query.get('temperature', 0.9) == 0:
+                                outputs = model.generate(
+                                    **inputs, do_sample=True, top_p=query.get('top_p', 0),
+                                    temperature=1.0, top_k=1,
+                                    max_new_tokens=query.get('max_tokens', 16),
+                                    return_dict_in_generate=True,
+                                    output_scores=True,  # return logit score
+                                    output_hidden_states=True,  # return embeddings
+                                )
+                            else:
+                                outputs = model.generate(
+                                    **inputs, do_sample=True, top_p=query.get('top_p', 0),
+                                    temperature=query.get('temperature', 0.9),
+                                    max_new_tokens=query.get('max_tokens', 16),
+                                    return_dict_in_generate=True,
+                                    output_scores=True,  # return logit score
+                                    output_hidden_states=True,  # return embeddings
+                                )
 
-                        texts = tokenizer.batch_decode(outputs.sequences, skip_special_tokens=True)
+                            current_output_texts = tokenizer.batch_decode(outputs.sequences, skip_special_tokens=True)
+                            answers.extend(current_output_texts)
+
                         end_time = time.time()
                         print(f"Job-{job_id} {args.model_name} Inference takes {end_time-start_time}s")
-                        print(f"Output: {texts[0]}")
                         # print(f"outputs by hf model: {outputs}")
-                        result = to_result(tokenizer.batch_decode(outputs.sequences, skip_special_tokens=True))
-
+                        result = to_result(raw_text,answers, args.model_name, query)
                         return_payload = {
                             'request': query,
                             'result': result,
