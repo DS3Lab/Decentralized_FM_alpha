461a6e03bf0b247cf12a61ac1b1d89f9732c1555
Jue Wang <zjuwangjue@gmail.com>
2022-10-21 11:30:34 +0000

    coordinator

--- run_lm3.sh
+++ run_lm3.sh
@@ -1,39 +1,84 @@
 
-for lr in 5
-do
+# --dp-mode local \
 
-for drop_iters in 10
-do
+# ARGS="--model-name ./checkpoints/gpt2 \
+# --tokenizer-name ./checkpoints/gpt2-tokenizer \
+# --load-pretrained-model true --warmup-epochs 1 --n-epochs 10 \
+# --task-name arxiv21 \
+# --num-layers 6 --num-heads 12 --embedding-dim 768 \
+# --num-iters 10000000 --lr 1e-4 --seq-length 1024 --batch-size 32 --micro-batch-size 1 \
+# --forward-compress-method none \
+# --backward-compress-method none \
+# --dist-url tcp://127.0.0.1:9033 \
+# --world-size 8 --pipeline-group-size 2 --data-group-size 4 \
+# --pp-mode gpipe --profiling no-profiling --do-evaluation true"
 
-for p in 0.02
+# (trap 'kill 0' SIGINT; \
+# python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 0 --rank 0 \
+#     & \
+# python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 1 --rank 1 \
+#     & \
+# python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 2 --rank 2 \
+#     & \
+# python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 3 --rank 3 \
+#     & \
+# python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 4 --rank 4 \
+#     & \
+# python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 5 --rank 5 \
+#     & \
+# python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 6 --rank 6 \
+#     & \
+# python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 7 --rank 7 \
+#     & \
+# wait)
+
+# # > /dev/null 2>&1 &
+
+PID=533359
+while [ -e /proc/$PID ]
 do
+    echo "Process: $PID is still running"
+    sleep .6
+done
+
+# --dp-mode local \
+
+job_id=`python3 -c 'import uuid; print(uuid.uuid4())'`
 
 ARGS="--model-name ./checkpoints/gpt2 \
 --tokenizer-name ./checkpoints/gpt2-tokenizer \
---load-pretrained-model true --warmup-epochs 1 --n-epochs 10 \
---task-name wikitext --seed 4242 \
---layer-drop-p ${p} \
---layer-drop-iters ${drop_iters} \
---layer-drop-method sample \
---num-layers 3 --num-heads 12 --embedding-dim 768 \
---num-iters 100000000 --lr ${lr}e-5 --seq-length 1024 --batch-size 32 --micro-batch-size 1 \
---dist-url tcp://127.0.0.1:9035 \
---world-size 4 --pipeline-group-size 4 --data-group-size 1 \
+--load-pretrained-model true --warmup-epochs 2 --n-epochs 10 \
+--task-name arxiv21 \
+--num-layers 6 --num-heads 12 --embedding-dim 768 \
+--num-iters 10000000 --lr 1e-4 --seq-length 1024 --batch-size 32 --micro-batch-size 1 \
+--forward-compress-method none \
+--backward-compress-method none \
+--dp-mode proxskip_adam \
+--fp16 --loss-scale 0 --initial-loss-scale 4096 --loss-scale-window 100 \
+--dist-url tcp://127.0.0.1:9033 \
+--world-size 8 --pipeline-group-size 2 --data-group-size 4 \
+--job-id ${job_id} --net-interface lo \
 --pp-mode gpipe --profiling no-profiling --do-evaluation true"
 
 (trap 'kill 0' SIGINT; \
-python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 0 --rank 0 \
-    &
-python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 1 --rank 1 \
-    &
-python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 2 --rank 2 \
-    &
-python dist_lm_runner.py $(echo ${ARGS}) --cuda-id 3 --rank 3 \
+python dist_lm_runner_w_coordinator.py $(echo ${ARGS}) --cuda-id 0 --rank 0 \
+    & \
+python dist_lm_runner_w_coordinator.py $(echo ${ARGS}) --cuda-id 1 --rank 1 \
+    & \
+python dist_lm_runner_w_coordinator.py $(echo ${ARGS}) --cuda-id 2 --rank 2 \
+    & \
+python dist_lm_runner_w_coordinator.py $(echo ${ARGS}) --cuda-id 3 --rank 3 \
+    & \
+python dist_lm_runner_w_coordinator.py $(echo ${ARGS}) --cuda-id 4 --rank 4 \
+    & \
+python dist_lm_runner_w_coordinator.py $(echo ${ARGS}) --cuda-id 5 --rank 5 \
+    & \
+python dist_lm_runner_w_coordinator.py $(echo ${ARGS}) --cuda-id 6 --rank 6 \
+    & \
+python dist_lm_runner_w_coordinator.py $(echo ${ARGS}) --cuda-id 7 --rank 7 \
     & \
 wait)
 
 # > /dev/null 2>&1 &
 
-done
-done
-done
+
