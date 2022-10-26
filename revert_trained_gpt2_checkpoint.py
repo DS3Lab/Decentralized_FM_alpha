122dad254cca0cba5dca454d054b7178ab9caf95
Jue Wang <zjuwangjue@gmail.com>
2022-10-26 15:28:38 +0000

    update revert checkpoint

--- revert_trained_gpt2_checkpoint.py
+++ revert_trained_gpt2_checkpoint.py
@@ -12,22 +12,26 @@ if __name__ == '__main__':
                         help='model-name')
     parser.add_argument('--output-path', type=str, default='model_checkpoints/gpt2-xl-prefix-result', 
                         help='model-name')
+    parser.add_argument('--n-stages', type=int, default=2, 
+                        help='')
+    parser.add_argument('--n-layer-per-stage', type=int, default=14, 
+                        help='')
     args = parser.parse_args()
     
     os.system(f'mkdir -p {args.output_path}')
     
-    n_stages = 4
-    n_layer_per_stage = 12
+    n_stages = args.n_stages
+    n_layer_per_stage = args.n_layer_per_stage
 
     for i in range(n_stages):
-        checkpoint = torch.load(os.path.join(input_path, f'prank_{i}_checkpoint.pt'))
+        checkpoint = torch.load(os.path.join(input_path, f'prank_{i}_checkpoint.pt'), map_location=torch.device("cpu"))
 
         if i == 0:
             _tmp = {k[len(f"{0}."):]:v for k,v in checkpoint.items() if k.startswith(f"0.")}
             torch.save(_tmp, os.path.join(output_path, f'pytorch_embs.pt'))
 
             for j in range(n_layer_per_stage):
-                _tmp = {k[len(f"{j}."):]:v for k,v in checkpoint.items() if k.startswith(f"{j+1}.")}
+                _tmp = {k[len(f"{j+1}."):]:v for k,v in checkpoint.items() if k.startswith(f"{j+1}.")}
                 if len(_tmp) == 0:
                     break
                 torch.save(_tmp, os.path.join(output_path, f'pytorch_{j}.pt'))
