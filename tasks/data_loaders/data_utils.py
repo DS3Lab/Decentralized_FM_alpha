7818a19433c3e3b1326627e1f7b913b6ecb8a1a4
Jue Wang <zjuwangjue@gmail.com>
2022-11-24 03:13:22 +0000

    update stream + multi worker

--- tasks/data_loaders/data_utils.py
+++ tasks/data_loaders/data_utils.py
@@ -205,7 +205,7 @@ class StreamDatasetList(IterableDataset):
     
     
     
-def get_train_data_loader(args, tokenizer, num_workers=0, state_dict=None):
+def get_train_data_loader(args, tokenizer, num_workers=1, state_dict=None):
     
     task_list = args.task_name.split(',')
     task_names = []
@@ -228,7 +228,7 @@ def get_train_data_loader(args, tokenizer, num_workers=0, state_dict=None):
             dataset = StreamDataset(data, tokenizer, args.seq_length)
         elif task == 'pile':
             from .pile import StreamDataset
-            data = load_dataset('the_pile', split="train", streaming=True).shuffle(buffer_size=10_000, seed=args.seed)
+            data = load_dataset('the_pile', split="train", streaming=True).shuffle(buffer_size=10_000, seed=args.seed).with_format("torch")
             # data = load_dataset('the_pile', split="train").shuffle(seed=args.seed)
             dataset = StreamDataset(data, tokenizer, args.seq_length)
         elif task == 'c4':
@@ -264,7 +264,7 @@ def get_train_data_loader(args, tokenizer, num_workers=0, state_dict=None):
 
 
 
-def get_ul2r_train_data_loader(args, tokenizer, num_workers=0, state_dict=None):
+def get_ul2r_train_data_loader(args, tokenizer, num_workers=1, state_dict=None):
     
     task_list = args.task_name.split(',')
     task_names = []
@@ -287,7 +287,7 @@ def get_ul2r_train_data_loader(args, tokenizer, num_workers=0, state_dict=None):
             dataset = StreamDataset(data, tokenizer, args.seq_length)
         elif task == 'pile':
             from .pile import StreamDataset
-            data = load_dataset('the_pile', split="train", streaming=True).shuffle(buffer_size=10_000, seed=args.seed)
+            data = load_dataset('the_pile', split="train", streaming=True).shuffle(buffer_size=10_000, seed=args.seed).with_format("torch")
             # data = load_dataset('the_pile', split="train").shuffle(seed=args.seed)
             dataset = StreamDataset(data, tokenizer, args.seq_length)
         elif task == 'c4':
