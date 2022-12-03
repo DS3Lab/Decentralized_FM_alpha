e944581d387d40caa2bdb22946ca167dac866a01
Jue Wang <yuanbinhang@hotmail.com>
2022-12-03 08:25:18 +0000

    update

--- slurms_scrips/train_template.lsf.sh
+++ slurms_scrips/train_template.lsf.sh
@@ -25,8 +25,8 @@ netif=access
 export GLOO_SOCKET_IFNAME=${netif}
 export NCCL_SOCKET_IFNAME=${netif}
 export NCCL_DEBUG=INFO
-# export NCCL_IB_DISABLE=1
-# export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
+export NCCL_P2P_DISABLE=1
 export WANDB_DISABLE_SERVICE=1
 
 root_path=/nfs/iiscratch-zhang.inf.ethz.ch/export/zhang/export/fm
@@ -39,12 +39,12 @@ ARGS="--model-name ${root_path}/pretrained_models/gpt-j-6B \
 --model-type gptj \
 --seed 42 \
 --checkpoint-path ${root_path}/pretrained_models/checkpoints/gptj-test \
---load-pretrained-model false \
+--load-pretrained-model true \
 --task-name /cluster/home/juewang/scratch/pile_1280k.jsonl:0.5,ni:0.5 \
 --num-layers ${n_layer_per_device} --num-heads 16 --embedding-dim 4096 \
 --total-steps 100000 --warmup-steps 100 --train-warmup-steps 0 \
 --checkpoint-steps 100 \
---lr 1e-5 --seq-length 2048 --batch-size 8 --micro-batch-size 1 --gradient-accumulate-step 1 \
+--lr 1e-5 --seq-length 2048 --batch-size 8 --micro-batch-size 1 --gradient-accumulate-step 2 \
 --dist-url tcp://127.0.0.1:9011 \
 --world-size ${world_size} --pipeline-group-size ${pp_degree} --data-group-size ${dp_degree} \
 --job-id ${job_id} --net-interface ${netif} \
