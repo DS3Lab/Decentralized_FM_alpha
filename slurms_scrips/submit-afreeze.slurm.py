0d89921537324fe5e733b618b67001fcaa2ec83e
Jue Wang <yuanbinhang@hotmail.com>
2022-12-11 15:54:56 +0000

    update

--- slurms_scrips/submit-afreeze.slurm.py
+++ slurms_scrips/submit-afreeze.slurm.py
@@ -3,7 +3,7 @@ import os
 import uuid
 
 template = '''#!/bin/bash
-#SBATCH --job-name=gpt_j_6b_20x
+#SBATCH --job-name=opt_1.3b
 #SBATCH --gpus=1 
 #SBATCH --gres=gpumem:20g
 #SBATCH --time=9:59:00
@@ -43,7 +43,7 @@ ARGS="--model-name ${root_path}/pretrained_models/opt-1.3b-new \
 --project-name loooofi \
 --model-type opt \
 --seed 4242 \
---checkpoint-path ${root_path}/pretrained_models/checkpoints/opt-afreeze-25x-new \
+--checkpoint-path ${root_path}/pretrained_models/checkpoints/opt-afreeze-25x-4x-new \
 --load-pretrained-model true \
 --task-name /cluster/home/juewang/scratch/pile_1280k.jsonl:0.5,ni:0.5 \
 --num-layers ${n_layer_per_device} --num-heads 32 --embedding-dim 2048 \
