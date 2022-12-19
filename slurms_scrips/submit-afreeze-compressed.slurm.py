d445121229978b732ed31129d12bbbf8bd100595
Jue Wang <yuanbinhang@hotmail.com>
2022-12-19 07:29:47 +0000

    update

--- slurms_scrips/submit-afreeze-compressed.slurm.py
+++ slurms_scrips/submit-afreeze-compressed.slurm.py
@@ -36,8 +36,8 @@ export WANDB_DISABLE_SERVICE=1
 
 export SYNC_STEPS=1
 export QUANT_BITS=8
-export QUANT_BUCKET_SIZE=8
-export TOPK_RATIO=0.01
+export QUANT_BUCKET_SIZE=128
+export TOPK_RATIO=0.008
 
 root_path=/nfs/iiscratch-zhang.inf.ethz.ch/export/zhang/export/fm
 
@@ -48,7 +48,7 @@ ARGS="--model-name ${root_path}/pretrained_models/opt-1.3b-new \
 --project-name loooofi \
 --model-type opt \
 --seed 4242 \
---checkpoint-path ${root_path}/pretrained_models/checkpoints/opt-afreeze-compressed-1x-in8-top1 \
+--checkpoint-path ${root_path}/pretrained_models/checkpoints/opt-afreeze-compressed-1x-in8-top0.8-fix \
 --load-pretrained-model true \
 --task-name /cluster/home/juewang/scratch/pile_1280k.jsonl:0.5,ni:0.5 \
 --num-layers ${n_layer_per_device} --num-heads 32 --embedding-dim 2048 \
