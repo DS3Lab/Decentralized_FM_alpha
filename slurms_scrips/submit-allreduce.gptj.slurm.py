286523a2890035d09c3b4c2c31ec635e12fb34a3
Jue Wang <yuanbinhang@hotmail.com>
2023-01-06 03:21:03 +0000

    update

--- slurms_scrips/submit-allreduce.gptj.slurm.py
+++ slurms_scrips/submit-allreduce.gptj.slurm.py
@@ -33,7 +33,7 @@ export NCCL_DEBUG=INFO
 export NCCL_IB_DISABLE=1
 export NCCL_P2P_DISABLE=1
 export WANDB_DISABLE_SERVICE=1
-export WANDB_NAME=gptj-allreduce-euler
+export WANDB_NAME=gptj-allreduce-jt
 export WANDB_ENTITY=pipeline-activation-compression
 
 export SYNC_STEPS=10
