af92b8dba674901d094c9f7f3305fcb1fc73998d
Xiaozhe Yao <askxzyao@gmail.com>
2023-01-29 17:46:04 +0100

    add gpt2

---
+++ api_finetune_gpt2.sh
@@ -0,0 +1,53 @@
+project_id=$PROJECT_ID
+dataset_url=$DATASET_URL
+
+echo $project_id
+
+export NCCL_DEBUG=INFO
+export WANDB_DISABLED=1
+huggingface-cli repo create ${project_id} -y
+git clone https://huggingface.co/xzyao/${project_id} ./model_checkpoints/${project_id}
+
+wget ${dataset_url} -O data/${project_id}.jsonl
+
+main_program=dist_lm_pretrain.py
+seed=$SEED
+total_steps=$TOTAL_STEPS
+warmup_steps=$WARMUP_STEPS
+train_warmup_steps=$TRAIN_WARMUP_STEPS
+learning_rate=$LEARNING_RATE
+seq_length=$SEQ_LENGTH
+gradient_accumulate_step=$GRADIENT_ACCUMULATE_STEP
+
+ARGS="--model-name /mnt/ds3lab-scratch/fm/pretrained_models/gpt2 \
+--tokenizer-name /mnt/ds3lab-scratch/fm/pretrained_models/gpt2 \
+--project-name ${project_id} \
+--model-type gpt2 \
+--seed ${seed} \
+--checkpoint-path ./model_checkpoints/${project_id} \
+--load-pretrained-model true \
+--task-name data/${project_id}.jsonl \
+--num-layers 6 --num-heads 32 --embedding-dim 768 \
+--total-steps ${total_steps} --warmup-steps ${warmup_steps} --train-warmup-steps ${train_warmup_steps} \
+--checkpoint-steps ${total_steps} \
+--lr ${learning_rate} --seq-length ${seq_length} --batch-size 5 --micro-batch-size 1 --gradient-accumulate-step ${gradient_accumulate_step} \
+--dist-url tcp://127.0.0.1:9011 \
+--world-size 2 --pipeline-group-size 2 --data-group-size 1 \
+--job-id ${project_id} --net-interface enp49s0f0 \
+--fp16 \
+--dp-mode allreduce \
+--pp-mode gpipe --profiling no-profiling"
+
+echo $ARGS
+
+(trap 'kill 0' SIGINT; \
+python ${main_program} $(echo ${ARGS}) --cuda-id 0 --rank 0 \
+    & \
+python ${main_program} $(echo ${ARGS}) --cuda-id 1 --rank 1 \
+    & \
+wait)
+
+cp /nlp/scr2/nlp/fmStore/cs324/pretrained_weights/gpt-j-6B/*.json ./model_checkpoints/${project_id}/
+cp /nlp/scr2/nlp/fmStore/cs324/pretrained_weights/gpt-j-6B/*.txt ./model_checkpoints/${project_id}/
+cp ./model_checkpoints/${project_id}/checkpoint_${total_steps}/* ./model_checkpoints/${project_id}/
+FINETUNE_ID=${project_id} TOTAL_STEPS=${total_steps} python tohub_gptj.py
---
+++ app/serve_finetune_gpt2.py
@@ -0,0 +1,68 @@
+import os
+import random
+import string
+import traceback
+import subprocess
+from typing import Dict
+
+from together_worker.fast_training import FastTrainingInterface
+from together_web3.together import TogetherWeb3, TogetherClientOptions
+
+class FinetuneGPT(FastTrainingInterface):
+    def __init__(self, model_name: str, args=None) -> None:
+        args = args if args is not None else {}
+        super().__init__(model_name, args)
+
+    def dispatch_request(self, args, env) -> Dict:
+        match_event = env
+        try:
+            project_id = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(50))
+            # call the finetune script
+            my_env = os.environ.copy()
+            my_env['PROJECT_ID'] = project_id
+            my_env['DATASET_URL'] = args[0]['dataset_url']
+            # set default arguments
+            my_env['SEED'] = '42'
+            my_env['TOTAL_STEPS'] = '100'
+            my_env['WARMUP_STEPS'] = '10'
+            my_env['TRAIN_WARMUP_STEPS'] = '0'
+            my_env['LEARNING_RATE'] = '1e-5'
+            my_env['SEQ_LENGTH'] = '1024'
+            my_env['GRADIENT_ACCUMULATE_STEP'] = '1'
+            for k in args[0]['arguments']:
+                my_env[k.upper()] = str(args[0]['arguments'][k])
+            if os.environ.get("CUDA_VISIBLE_DEVICES") is None:
+                my_env['CUDA_VISIBLE_DEVICES']="0,1"
+            proc = subprocess.Popen(
+                "/bin/bash api_finetune_gpt2.sh",
+                shell=True,
+                env=my_env
+            )
+            self.update_step({
+                "step": 0,
+                "finetune_id": project_id,
+            }, match_event)
+            proc.wait()
+            return {"finetune_id": project_id}
+        except Exception as e:
+            traceback.print_exc()
+
+if __name__ == "__main__":
+    coord_url = os.environ.get("COORD_URL", "localhost")
+    coordinator = TogetherWeb3(
+        TogetherClientOptions(reconnect=True),
+        http_url=os.environ.get("COORD_HTTP_URL", f"http://{coord_url}:8092"),
+        websocket_url=os.environ.get("COORD_WS_URL", f"ws://{coord_url}:8093/websocket"),
+    )
+    fip = FinetuneGPT(model_name=os.environ.get("MODEL", "FT_GPTJ-6B"), args={
+        "auth_token": os.environ.get("AUTH_TOKEN"),
+        "coordinator": coordinator,
+        "device": os.environ.get("DEVICE", "cuda"),
+        "gpu_num": 0,
+        "gpu_type": 'titanxp',
+        "stream_tokens_pipe": True,
+        "gpu_mem": 12,
+        "group_name": os.environ.get("GROUP", "group1"),
+        "worker_name": os.environ.get("WORKER", "worker1"),
+    })
+    fip.start()
