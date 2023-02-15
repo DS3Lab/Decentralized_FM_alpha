9b05b599ad7683951bb64ff6f909b90bdb428ece
Xiaozhe Yao <askxzyao@gmail.com>
2023-02-16 00:53:53 +0100

    fix typo

--- tohub.py
+++ tohub.py
@@ -1,26 +1,5 @@
 import os
-from transformers import OPTForCausalLM, AutoTokenizer, AutoModelForCausalLM
-
-print("Uploading model to hub")
-finetune_id = os.environ.get("FINETUNE_ID")
-finetune_path = os.path.join("model_checkpoints", finetune_id)
-
-model = AutoModelForCausalLM.from_pretrained(finetune_path)
-
-tokenizer = AutoTokenizer.from_pretrained(finetune_path)
-
-tokenizer.push_to_hub(
-    repo_path_or_name=f"./model_checkpoints/{finetune_id}",
-    repo_url=f"https://huggingface.co/xzyao/{finetune_id}",
-    use_auth_token=True,
-)
-model.push_to_hub(
-    repo_path_or_name=f"./model_checkpoints/{finetune_id}",
-    repo_url=f"https://huggingface.co/xzyao/{finetune_id}",
-    use_auth_token=True,
-)
-
-
+from transformers import OPTForCausalLM, AutoTokenizer
 import torch
 import torch.nn as nn
 
@@ -29,7 +8,6 @@ from transformers import OPTForCausalLM
 from transformers import AutoConfig, AutoTokenizer
 
 from transformers.modeling_utils import no_init_weights
-import os
 
 
 def create_emtpy_opt(config):
